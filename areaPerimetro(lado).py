dcea19a8d507e946f1afdbc7ba8ad450d643ab5b
Jacinto <jacinto.tc27@gmail.com>
2025-03-13 18:17:08 -0600

    Add files via upload

---
+++ areaPerimetro(lado).py
@@ -0,0 +1,11 @@
+def CalcularPerimetro ():
+    lado = int(input("introduce la cantdiad correspondientes al perimetros:"))
+    if lado <=0:
+        print("El valor ingresado no es valido")
+    else:
+     perimetro = lado*4
+     print (f"el perimetro es:{perimetro}")
+    
+    
+    
+        
---
+++ areaRectangulo(base,altura).py
@@ -0,0 +1,5 @@
+base = float(input("introduce la base del rectangulo:"))
+altura = float(input("introduce la altura del rectangulo:"))
+Area = base*altura
+print("El area del rectangulo es:", Area)
+
